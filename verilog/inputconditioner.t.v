bcd000a27a682dff5f8780d648d457f5225ba3c8
David Abrahams <david.abrahams@students.olin.edu>
2015-10-29 23:31:28 -0400

    wrote test bench for input conditioner. completed input conditioner

---
+++ verilog/inputconditioner.t.v
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------
+// Input Conditioner test bench
+//------------------------------------------------------------------------
+
+module testConditioner();
+
+    reg clk;
+    reg pin;
+    wire conditioned;
+    wire rising;
+    wire falling;
+
+    inputconditioner dut(.clk(clk),
+    			 .noisysignal(pin),
+			 .conditioned(conditioned),
+			 .positiveedge(rising),
+			 .negativeedge(falling));
+
+
+    // Generate clock (50MHz)
+    initial clk=0;
+    always #10 clk=!clk;    // 50MHz Clock
+
+    initial begin
+
+        $dumpfile("test.vcd");
+        $dumpvars(0,dut);
+
+        $display("pin conditioned rising falling | Expected");
+        // input sync. Changing input 7 seconds into clock cycle. Checking
+        // output 123 seconds afterward (on clock cycle). Output is
+        // synchronized.
+
+        $display("Testing input sync");
+        pin = 0; #70
+        #7
+        pin = 1; #123
+        $display("%b   %b           %b      %b       | %b %b %b %b", pin,
+                 conditioned, rising, falling, 1'b1, 1'b1, 1'b1, 1'b0);
+        // Debouncing. Signal switches rapidly for 184 seconds. COnditioned
+        // remains constant.
+
+        $display("Testing debouncing");
+        pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8
+        pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8
+        pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8
+        pin = 0; #8 pin = 1; #8 pin = 0; #8 pin = 1; #8 pin = 0; #8
+        $display("%b   %b           %b      %b       | %b %b %b %b", pin,
+                 conditioned, rising, falling, 1'b0, 1'b1, 1'b0, 1'b0);
+        #6
+
+        // Edge detection.
+        $display("Testing edge detection: falling");
+        pin = 1; #20
+        pin = 0; #110
+        $display("%b   %b           %b      %b       | %b %b %b %b", pin,
+                 conditioned, rising, falling, 1'b0, 1'b0, 1'b0, 1'b1);
+
+        $display("Testing edge detection: rising");
+        #10
+        pin = 1; #110
+        $display("%b   %b           %b      %b       | %b %b %b %b", pin,
+                 conditioned, rising, falling, 1'b1, 1'b1, 1'b1, 1'b0);
+        #10
+        $finish;
+    end
+
+    // Your Test Code
+    // Be sure to test each of the three conditioner functions:
+    // Synchronize, Clean, Preprocess (edge finding)
+
+endmodule
--- verilog/inputconditioner.v
+++ verilog/inputconditioner.v
@@ -16,21 +16,33 @@ output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
 
     parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
     parameter waittime = 3;     // Debounce delay, in clock cycles
-    
+
     reg[counterwidth-1:0] counter = 0;
     reg synchronizer0 = 0;
     reg synchronizer1 = 0;
-    
+
     always @(posedge clk ) begin
+
+        if (positiveedge || negativeedge) begin
+            positiveedge <= 0;
+            negativeedge <= 0;
+        end
+
         if(conditioned == synchronizer1)
             counter <= 0;
+
+
+
         else begin
             if( counter == waittime) begin
+                positiveedge <= synchronizer1;
+                negativeedge <= !synchronizer1;
                 counter <= 0;
                 conditioned <= synchronizer1;
             end
-            else 
+            else begin
                 counter <= counter+1;
+            end
         end
         synchronizer0 <= noisysignal;
         synchronizer1 <= synchronizer0;

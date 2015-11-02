adeb6b44a13ebe2f29dd01c4d62fc8fe34bcc8cb
David Abrahams <david.abrahams@students.olin.edu>
2015-11-01 20:21:59 -0500

    labeled test bench

--- tests/shiftregister.t.v
+++ tests/shiftregister.t.v
@@ -28,16 +28,20 @@ module testshiftregister();
       $dumpfile("shftregister.vcd");
       $dumpvars(0,dut);
 
+
       $display("-----------------------------------------------");
+      $display("Testing parallel in, serial out.");
       $display("parallelLoad | parallelDataIn | serialDataOut | Expected Out");
       parallelLoad = 1; parallelDataIn = 8'b10111010; #20
       $display("%b            | %b       | %b             | 1", parallelLoad, parallelDataIn, serialDataOut);
       $display("-----------------------------------------------");
       parallelLoad = 0;
+      $display("Testing serial in, parallel out.");
       $display("peripheralClkEdge | serialDataIn | parallelDataOut | Expected Out");
       peripheralClkEdge = 1; serialDataIn = 1; #20
       $display("%b                 | %b            | %b        | 01110101", peripheralClkEdge, serialDataIn, parallelDataOut);
       $display("-----------------------------------------------");
+      $display("Testing parallel load and peripheralClkEdge. peripheralClkEdge wins.");
       parallelLoad = 1;
       $display("parallelLoad | peripheralClkEdge | parallelDataIn | parallelDataOut | Expected Out");
       parallelDataIn = 8'b0; serialDataIn =  0; #20
